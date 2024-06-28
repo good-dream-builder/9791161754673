2a691eb67fd94c1bce777e4ee0201e1ed204e417
psj <good.dream.builder@gmail.com>
2024-06-28 17:57:46 +0900

    11장 토큰 발행 실패로 분석 중.

---
+++ cloud/gateway/src/main/java/com/songko/gateway/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.songko.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Slf4j
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/headerrouting/**").permitAll()
+                .pathMatchers("/actuator/**").permitAll()
+                .pathMatchers("/eureka/**").permitAll()
+                .pathMatchers("/oauth/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
